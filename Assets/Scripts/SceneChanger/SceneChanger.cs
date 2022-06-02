e445e1327dd68ca842f462d5e07b5b3ad0af5fab
Demionchek <Dpcustom@yandex.ru>
2022-06-02 11:56:49 +0300

    Added Continue Button

--- Assets/Scripts/SceneChanger/SceneChanger.cs
+++ Assets/Scripts/SceneChanger/SceneChanger.cs
@@ -1,11 +1,31 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneChanger : MonoBehaviour
 {
     private const string firstLevelSceneName = "Village";
     private const string mainMenuSceneName = "MainMenu";
     private const string winSceneName = "WinScene";
+    [SerializeField] private Button _continueButton;
+    [SerializeField] private Saver _saver;
+    private void Start()
+    {
+        if (_saver.LevelToSave != 0)
+        {
+            _continueButton.interactable = true;
+        }
+        else
+        {
+            _continueButton.interactable = false;
+        }
+    }
+
+    public void Continue()
+    {
+        int sceneToLoad = _saver.LevelToSave;
+        SceneManager.LoadScene(sceneToLoad);
+    }
 
     public void LoadFirstScene()
     {
