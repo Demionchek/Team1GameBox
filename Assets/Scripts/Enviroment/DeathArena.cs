608f3101ec4f19db76528db4334a4904ab1f5a15
Demionchek <Dpcustom@yandex.ru>
2022-06-06 23:23:03 +0300

    Added Arena Wall

--- Assets/Scripts/Enviroment/DeathArena.cs
+++ Assets/Scripts/Enviroment/DeathArena.cs
@@ -6,9 +6,9 @@ using StarterAssets;
 public class DeathArena : MonoBehaviour
 {
     [SerializeField] private GameObject _barier;
+    [SerializeField] private GameObject _forestWall; 
     [SerializeField] private EnemySpawner spawner;
     private bool _isActive;
-    private SphereCollider _collider;
 
     private const float k_delay = 1f;
     public int EnemiesAlive { get; set; }
@@ -16,7 +16,6 @@ public class DeathArena : MonoBehaviour
     private void Start()
     {
         _barier.SetActive(false);
-        _collider = GetComponent<SphereCollider>();
     }
 
     private void DeathCounter()
@@ -31,7 +30,6 @@ public class DeathArena : MonoBehaviour
         _isActive = true;
         _barier.SetActive(true);
         StartCoroutine(CheckIfEnemiesAreAlive());
-        _collider.enabled = false;
     }
 
     private IEnumerator CheckIfEnemiesAreAlive()
@@ -43,6 +41,7 @@ public class DeathArena : MonoBehaviour
             if (EnemiesAlive <= 0)
             {
                 _isActive = false;
+                _forestWall.SetActive(false);
             }
         }
         _barier.SetActive(false);
