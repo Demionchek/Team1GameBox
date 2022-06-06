55b26b823c32afd4f38643cf5b9db46c6878f3a5
Demionchek <Dpcustom@yandex.ru>
2022-06-06 18:01:11 +0300

    Added Dash And Slam Effects

--- Assets/Scripts/Hero/PlayerEffects.cs
+++ Assets/Scripts/Hero/PlayerEffects.cs
@@ -6,12 +6,25 @@ public class PlayerEffects : MonoBehaviour
     [SerializeField] private VisualEffect _firstSlashEffect;
     [SerializeField] private VisualEffect _secondSlashEffect;
     [SerializeField] private VisualEffect _thirdlashEffect;
+    [SerializeField] private VisualEffect _dashEffect;
+    [SerializeField] public VisualEffect GroundSlam;
 
     private void Start()
     {
         _firstSlashEffect.Stop();
         _secondSlashEffect.Stop();
         _thirdlashEffect.Stop();
+        _dashEffect.Stop();
+    }
+
+    public void PlayDashEffect()
+    {
+        _dashEffect.Play();
+    }
+
+    public void StopDashEffect()
+    {
+        _dashEffect.Stop();
     }
 
     public void FirstSlashEffect()
