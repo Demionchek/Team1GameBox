2c0d6385402e840bb08f185a787145a9a217b6c5
Demionchek <Dpcustom@yandex.ru>
2022-06-06 20:49:09 +0300

    AddedTriggers

---
+++ Assets/Scripts/Enemy/EnemyTriggers.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class EnemyTriggers : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _packs;
+    [SerializeField] private Saver _saver;
+    [SerializeField] private int nextCheckPoint;
+    private bool _spawn;
+
+    private void Start()
+    {
+        _saver.LoadCheckPoint();
+        int i = _saver.CheckPointToSave;
+        if (i < nextCheckPoint)
+        {
+            Debug.Log("AllOff");
+            _spawn = true;
+            foreach (var pack in _packs)
+            {
+                pack.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out CharacterController controller))
+        {
+            Debug.Log("enter");
+            if (_spawn)
+            {
+                foreach (var pack in _packs)
+                {
+                    pack.SetActive(true);
+                }
+                _spawn = false;
+            }
+        }
+    }
+}
