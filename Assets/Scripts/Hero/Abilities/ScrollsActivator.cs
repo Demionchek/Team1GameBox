f0801e8d0b342bee0ee6118e5145d256b6c75d35
Demionchek <Dpcustom@yandex.ru>
2022-06-22 15:24:05 +0300

    Stump Animations replaced, fixed player push

--- Assets/Scripts/Hero/Abilities/ScrollsActivator.cs
+++ Assets/Scripts/Hero/Abilities/ScrollsActivator.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class ScrollsActivator : MonoBehaviour
 {
-    [SerializeField] private Saver _saver;
-    [SerializeField] private GameObject scroll1;
-    [SerializeField] private GameObject scroll2;
-    [SerializeField] private GameObject scroll3;
-    [SerializeField] private GameObject scroll4;
+    [SerializeField] private SavingSystem _saveSystem;
+    [SerializeField] private UnlockItems[] _scrolls;
 
     void Start()
     {
-        _saver.LoadScrollsNums();
-        if (_saver.FirstScroll == 1)
-            scroll1.SetActive(false);
-        if (_saver.SecondScroll == 1)
-            scroll2.SetActive(false);
-        if (_saver.ThirdScroll == 1)
-            scroll3.SetActive(false);
-        if(_saver.FourthScroll == 1)
-            scroll4.SetActive(false);
+        if (_scrolls != null)
+        {
+            foreach (var scroll in _scrolls)
+            {
+                
+            }
+        }
     }
 }
--- Assets/Scripts/Hero/Abilities/UnlockSystem/UnlockItems.cs
+++ Assets/Scripts/Hero/Abilities/UnlockSystem/UnlockItems.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class UnlockItems : MonoBehaviour
 {
-    [SerializeField,Range(0,3)] private int _scrollNum;
+    [SerializeField,Range(0,3)] public int ScrollNum;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<UnlockSystemManager>(out UnlockSystemManager unlockManager))
         {
-            unlockManager.TryUnlock(_scrollNum);
+            unlockManager.TryUnlock(ScrollNum);
             Destroy(gameObject);
         }
     }
